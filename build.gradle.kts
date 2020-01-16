cb1f21888c9025ded0e8fcab38122f4d7ca03ec7
Philip Crotwell <crotwell@seis.sc.edu>
2020-01-16 12:41:00 -0500

    switch to kotlin build

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    // Apply the java-library plugin to add support for Java Library
+    `java-library`
+
+}
+
+group = "edu.sc.seis"
+version = "1.0.12-SNAPSHOT"
+
+repositories {
+    // Use jcenter for resolving dependencies.
+    // You can declare any Maven/Ivy/file repository here.
+    jcenter()
+}
+
+dependencies {
+
+    // Use JUnit Jupiter API for testing.
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
+
+    // Use JUnit Jupiter Engine for testing.
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes(
+                mapOf("Implementation-Title" to project.name,
+                      "Implementation-Version" to project.version)
+            )
+        }
+    }
+}
+
+val test by tasks.getting(Test::class) {
+    // Use junit platform for unit tests
+    useJUnitPlatform()
+}
